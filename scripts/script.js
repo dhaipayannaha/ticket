0bc0a398ab7cb7a31c2ea641d6567b0a1c0d2b84
DESKTOP-U096J61\dhaip <dhaipayannahadipya@gmail.com>
2024-12-14 02:19:07 +0600

    tiket two

--- scripts/script.js
+++ scripts/script.js
@@ -1,98 +1,95 @@
+const seates = document.getElementsByClassName('seates');
+let count = 40;
+let countSeat = 0;
 
-let total = 0;
+for (const seat of seates) {
+    // console.log(seat);
+    seat.addEventListener('click', (e) => {
+        
+        count -= 1;
+        countSeat++;
 
-function pressed(set) {
-    console.log(set);
-
-    const div = document.getElementById("payment");
-
-    const ul = document.createElement('ul');
-    ul.classList.add('flex', 'justify-between');
-
-    let li4 = document.createElement('li');
-    li4.classList.add('text-black');
-    li4.innerText = set;
-
-
-    let li5 = document.createElement('li')
-    li5.innerText = 'Economy';
-    li5.classList.add('text-black');
-
-    let li6 = document.createElement('li');
-    li6.innerText = 550; // Add the ticket price here
-    li6.classList.add('text-black');
+        const seatName = e.target.innerText;
+        console.log(seatName);
 
 
-    div.appendChild(ul);
-    ul.appendChild(li4);
-    ul.appendChild(li5);
-    ul.appendChild(li6);
+        const priceList = document.getElementById("priceList");
+        const ul = document.createElement("ul");
+
+        const liSeat = document.createElement("li");
+        liSeat.innerText = seatName;
+
+        const liClass = document.createElement("li");
+        liClass.innerText = "Econmy";
+
+        const liPrice = document.createElement("li");
+        liPrice.innerText = 550;
 
 
-    tikla(set)
-    // Add the ticket price to the total
-    const ticketPrice = parseInt(li6.innerText);
-    total += ticketPrice;
+        ul.appendChild(liSeat);
+        ul.appendChild(liClass);
+        ul.appendChild(liPrice);
+        priceList.appendChild(ul);
 
-    
-
-    const totalPrice = document.getElementById('total');
-    totalPrice.innerText = total
-
-    console.log(total);
-
-    // unlock input field
-    document.getElementById('copon').disabled = false;
+        const seatSet = document.getElementById("count-seat");
+        seatSet.innerText = countSeat
 
 
-    
+        const totalCast = document.getElementById("total").innerText;
+        const convertedTotalCast = parseInt(totalCast);
+        const sumTotal = convertedTotalCast + parseInt(liPrice.innerText);
+
+        apply(sumTotal)
+
+        setInnerText("total", sumTotal);
+
+
+        setInnerText("left-seat", count);
+
+        e.target.classList.add('disabled');
+    })
+
 
 }
-    
-   
 
 
-function catched() {
-    const input = document.getElementById('copon');
-    let inputValue = input.value;
-    const grandTotal = document.getElementById('grandTotal');
-    let discount = 0;
+function apply() {
+    const copon = document.getElementById("copon").value;
 
-    if (inputValue === 'NEWS15') {
-        discount = total * 0.15;
 
-    } else if (inputValue === 'Couple20') {
-        discount = total * 0.20;
+
+    if (copon === 'Couple20') {
+        grandTotal("total", 0.20);
+    } else if (copon === 'NEWS15') {
+        grandTotal("total", 0.15);
     }
-
-    const grandTotalElement = total - discount;
-    grandTotal.innerText = grandTotalElement;
-
-
 }
 
-function tikla(ki) {
-    const selected = document.getElementById(ki);
-        selected.classList.add('bg-green-500', 'disabled')
-}
+// this is grand total area  /////
+function grandTotal(id, num) {
+    const totalCast = document.getElementById(id).innerText;
+    const convertedTotalCast = parseInt(totalCast);
 
+    const ttt = convertedTotalCast * num;
+    const fixed = convertedTotalCast - ttt;
 
-
-function hide (display){
-    const element = document.getElementById(display);
-    element.classList.add = "hidden";
-}
-
-function show (display){
-    const element = document.getElementById(display);
-    element.classList.remove = "hidden";
+    setInnerText('grandTotal', fixed);
 }
 
 
 
+//  this function set inner text
+function setInnerText(id, value) {
+    document.getElementById(id).innerText = value;
+}
 
 
-
+function buy() {
+    const showElement = document.getElementById("mainSection");
+    showElement.classList.add("hidden");
+    const hideElement = document.getElementById("payment");
+    hideElement.classList.remove("hidden");
+}
 
 
 
